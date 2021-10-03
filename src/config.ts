d1ac9706a04dc01e3c371bb7db6444efbb141e37
mourabraz <cmourabraz@gmail.com>
2021-10-03 22:58:40 +0100

    ch: remove unnecessary file

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('../../../config.pagination');
-export const defautValues = {
-  page: 1,
-  limit: 5,
-  maxLimit: 200,
-  ...config,
-};
