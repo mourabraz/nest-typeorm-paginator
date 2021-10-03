919519cf976c58f4183835c07555eea794e15c28
mourabraz <cmourabraz@gmail.com>
2021-10-03 23:02:14 +0100

    fix: script files

--- build-script.js
+++ build-script.js
@@ -2,14 +2,12 @@ const fs = require('fs');
 
 fs.writeFileSync(
   './src/config.ts',
-  `
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+  `// eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('../../../config.pagination');
 export const defautValues = {
   page: 1,
   limit: 5,
   maxLimit: 200,
   ...config,
-};
-`,
+};`,
 );
